b30ee5e71cd765a8e42bf3fe906cd8d71f125191
Dorje <rtorchiiw@gmail.com>
2018-09-04 11:43:16 +0545

    add urlpatterns

--- uptrack_app/urls.py
+++ uptrack_app/urls.py
@@ -1,14 +1,17 @@
-from rest_framework import routers
+from rest_framework_nested import routers
 from django.conf.urls import url, include
 from uptrack_app.viewset import ProjectViewSet, UpdateViewSet
 
-router = routers.DefaultRouter()
+router = routers.SimpleRouter()
 router.register(r'projects', ProjectViewSet,base_name='projects')
-router.register(r'updates', UpdateViewSet, base_name='updates')
+projects_router = routers.NestedSimpleRouter(router, r'projects', lookup='project')
+projects_router.register(r'updates', UpdateViewSet, base_name='project-updates')
+
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 
 urlpatterns = [
     url(r'^', include(router.urls)),
+    url(r'^', include(projects_router.urls)),
     url(r'^api/', include('rest_framework.urls', namespace='rest_framework'))
 ]
