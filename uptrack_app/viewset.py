0421d2170b3e354b27a02e0131d5cd883aaabdb2
Dorje <rtorchiiw@gmail.com>
2018-09-06 10:56:08 +0545

    UPdateCount api view added

--- uptrack_app/viewset.py
+++ uptrack_app/viewset.py
@@ -2,6 +2,8 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from uptrack_app.models import Project, Update
 from uptrack_app.serializers import ProjectSerializer, UpdateSerializer
+from rest_framework.views import APIView
+from django.db.models import Count
 
 class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
@@ -14,3 +16,8 @@ class UpdateViewSet(viewsets.ModelViewSet):
         queryset = Update.objects.filter( project = project_pk )
         serializer = UpdateSerializer( queryset, many = True )
         return Response(serializer.data)
+
+class UpdateCountView(APIView):
+    def get(self, request):
+        count = Update.objects.values("project__name").annotate(update = Count("project"))
+        return Response(count)
